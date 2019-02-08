b05e7b6d1cf37f807fd6f4d77c3d1c5f9733c62b
Glushakov Denis Vladimirovich <denis.glushakov@office.lamoda.ru>
2019-02-08 14:35:09 +0300

    8-2-2019

--- src/Controller/SectorManager.php
+++ src/Controller/SectorManager.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 
 use App\Entity\Attendance;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -15,16 +14,31 @@ class SectorManager extends AbstractController
      * @Route("/SectorManagerWorkSpace", name="SMworkInterface")
      *
      */
-    public function WorkInterface(){
+    public function WorkInterface()
+    {
 
-       // $entityManager = $this->getDoctrine()->getManager();
+        // $entityManager = $this->getDoctrine()->getManager();
         $attendanceRepo = $this->getDoctrine()->getRepository(Attendance::class);
         //$attendances = $attendanceRepo->findAll();
-        $attendances = $attendanceRepo->findBy( [],['dateTime'=>'DESC'],10);
+        $attendances = $attendanceRepo->findBy([], ['dateTime' => 'DESC'], 1010);
+
+
+        $file_counter = "counter.txt";
+        if (file_exists($file_counter) && filesize($file_counter) != 0) {
+            $fp = fopen($file_counter, "r");
+            $peopleCounter = fread($fp, filesize($file_counter));
+            fclose($fp);
+        } else {
+            $fp = fopen($file_counter, "w");
+            fwrite($fp, "0");
+            fclose($fp);
+            $peopleCounter = 0;
+        }
+
 
         $result = $this->render('SectorManager/SectorManagerInterface.html.twig', [
-            'attendances'=>$attendances,
-
+            'attendances' => $attendances,
+            'peopleCounter' => $peopleCounter,
         ]);
         return $result;
     }
@@ -33,33 +47,48 @@ class SectorManager extends AbstractController
     /**
      * @Route("/attendance/new/{login}", name ="AddAttendance")
      */
-    public function AddAttendance($login) {
+    public function AddAttendance($login)
+    {
         $entityManager = $this->getDoctrine()->getManager();
         $attendanceRepo = $this->getDoctrine()->getRepository(Attendance::class);
-        $lastLoginAttendance = $attendanceRepo->findOneBy(['login'=>$login], ['dateTime'=>'DESC']);
+        $lastLoginAttendance = $attendanceRepo->findOneBy(['login' => $login], ['dateTime' => 'DESC']);
 
+        $login = trim($login);
         $attendance = new Attendance();
         $attendance->setLogin($login);
         $attendance->setDateTime(new \DateTime());
 
-        if ($lastLoginAttendance && $lastLoginAttendance->getDirection()!='exit'){
+        $file_counter = "counter.txt";
+        $fp = fopen($file_counter, "r+");
+        $counter = (int)fread($fp, filesize($file_counter));
+        fclose($fp);
+
+        if ($lastLoginAttendance && $lastLoginAttendance->getDirection() != 'exit') {
             $attendance->setDirection('exit');
+            $counter--;
         } else {
             $attendance->setDirection('entrance');
+            $counter++;
         }
+        $fp = fopen($file_counter, "w");
+        fwrite($fp, $counter);
+        fclose($fp);
 
 
         $entityManager->persist($attendance);
         $entityManager->flush();
 
         //return new Response('Saved new product with id '.$attendance->getId());
+
+        //TODO изменение счетчика
         return $this->redirectToRoute('SMworkInterface');
     }
 
     /**
      * @Route("/editcomment/{attendanceId}/{text}")
      */
-    public function EditAttendanceComment($attendanceId, $text=null){
+    public function EditAttendanceComment($attendanceId, $text = null)
+    {
         $entityManager = $this->getDoctrine()->getManager();
 
         $attendanceRepo = $this->getDoctrine()->getRepository(Attendance::class);
@@ -75,7 +104,8 @@ class SectorManager extends AbstractController
     /**
      * @Route("/editfine/{attendanceId}/{action}")
      */
-    public function EditFine($attendanceId, $action=null){
+    public function EditFine($attendanceId, $action = null)
+    {
         $entityManager = $this->getDoctrine()->getManager();
 
         $attendanceRepo = $this->getDoctrine()->getRepository(Attendance::class);
@@ -88,13 +118,16 @@ class SectorManager extends AbstractController
         return $this->redirectToRoute('SMworkInterface');
     }
 
+
     /**
-     * @Route("/addcounter")
+     * @Route("/resetPeopleCounter", name ="resetPeopleCounter")
      */
-    public function addCounter() {
+    public function resetPeopleCounter()
+    {
 
-
-        return new JsonResponse(['counter' => rand(5, 100)]);
+        $file_counter = "counter.txt";
+        $fp = fopen($file_counter, "w");
+        fwrite($fp, "0");
+        return $this->redirectToRoute('SMworkInterface');
     }
-
 }
