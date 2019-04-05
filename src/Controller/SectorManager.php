7a6c1d874a624f918ea3b0f52c5e3467fd667744
Glushakov Denis Vladimirovich <denis.glushakov@office.lamoda.ru>
2019-04-05 16:53:41 +0300

    manually deleted delted form sector manager's interfase

--- src/Controller/SectorManager.php
+++ src/Controller/SectorManager.php
@@ -17,27 +17,35 @@ class SectorManager extends AbstractController
     public function WorkInterface()
     {
         $this->denyAccessUnlessGranted('ROLE_SECTOR_MANAGER');
-        // $entityManager = $this->getDoctrine()->getManager();
+
         $attendanceRepo = $this->getDoctrine()->getRepository(Attendance::class);
-//        $attendances = $attendanceRepo->findAll();
+
         //$attendances = $attendanceRepo->findUsersOnSectorInShift($this->getUser()->getSector(),$this->getUser()->getShift());
         //$attendances = $attendanceRepo->findUsersOnSector($this->getUser()->getSector());
         $attendances = $attendanceRepo->findUsersOnSectorExceptManuallyDeleted($this->getUser()->getSector());
         //TODO использовать другую выборку
 
         $attendancesOutput=[];
-        $lastLogin ="";
-        foreach ($attendances as $attendance ) {
-            if ($attendance->getLogin()!=$lastLogin && $attendance->getDirection()=='entrance'){
-                $attendancesOutput[]=$attendance;
-            }
-           $lastLogin=$attendance->getLogin();
-       }
+       $lastLogin ="";
+//        foreach ($attendances as $attendance ) {
+//            if ($attendance->getLogin()!=$lastLogin && $attendance->getDirection()=='entrance'){
+//                $attendancesOutput[]=$attendance;
+//            }
+//           $lastLogin=$attendance->getLogin();
+//       }
 
+        foreach ($attendances as $attendance ) {
+            if (!isset($attendancesOutput[$attendance->getLogin()])){
+                $attendancesOutput[$attendance->getLogin()]=$attendance;
+            }
+            $lastLogin=$attendance->getLogin();
+        }
+
+       // dd($attendancesOutput);
 
         $result = $this->render('SectorManager/SectorManagerInterface.html.twig', [
             'attendances' => $attendancesOutput,
-            'peopleCounter' => count($attendancesOutput),
+            //'peopleCounter' => count($attendancesOutput),
         ]);
         return $result;
     }
